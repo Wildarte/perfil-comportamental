1ec70dde29cffc327221fe79bf10f25f0541f0bb
wildarte <wildarte10@gmail.com>
2022-02-05 20:01:36 -0300

    commitei uma parte grande

--- index.php
+++ index.php
@@ -5,35 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste - Perfil comportamental</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     
-    <header>
-        <h1>Teste Disc</h1>
-        <h2>Avaliação de Perfil Comportamental gratuito.</h2>
+    <header class="header_top_area">
+        <h1 class="header_title">Teste Disc</h1>
+        <h2 class="header_subtitle">Avaliação de Perfil Comportamental gratuito.</h2>
     </header>
-    <section>
-        <div>
-            <div>
-                <img src="" alt="">
+    <section class="page_bottom">
+        <div class="container_wizard">
+            <div class="wizard_top">
+                <img src="assets/img/barra-disc.png" alt="">
             </div>
-            <div>
-                <header>
-                    <h3>Você está pronta(o) para conhecer seu perfil comportamental predominante?</h3>
+            <div class="wizard_text">
+                <header class="header_wizard_text">
+                    <h3 class="wizard_title">Você está pronta(o) para conhecer seu perfil comportamental predominante?</h3>
                 </header>
-                <div>
-                    <div>
-                        <h4>DICAS:</h4>
-                        <ol>
+                <div class="wizard_body">
+                    <div class="wizard_tips">
+                        <h4 class="tips_title">DICAS:</h4>
+                        <ol class="tips_list">
                             <li>Não há resultado bom ou ruim, por isso, seja sincera(o).</li>
                             <li>Escolha um momento tranquilo para fazer o teste.</li>
                             <li>Não pense muito, a primeira resposta geralmente será a mais correta.</li>
                             <li>Responda pensando em quem você é e não em quem gostaria de ser.</li>
                         </ol>
                     </div>
-                    <div>
-                        <h4>Clique no botão para começar</h4>
-                        <button>Carregando...</button>
+                    <div class="wizard_action">
+                        <h4 class="wizard_action_title">Clique no botão!</h4>
+                        <button class="btn_wizard" id="btn_wizard">Carregando...</button>
                     </div>
                 </div>
             </div>
@@ -42,6 +44,12 @@
             <img src="" alt="">
         </div>
     </section>
+
+    <div class="social_share">
+        <div>
+            <span>Partilhar</span>
+        </div>
+    </div>
 
 </body>
 </html>
