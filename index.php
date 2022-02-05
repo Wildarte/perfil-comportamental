8a1ca05c565f7e570adc60dce8d38bba39470b88
wildarte <wildarte10@gmail.com>
2022-02-05 17:56:55 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste - Perfil comportamental</title>
+</head>
+<body>
+    
+    <header>
+        <h1>Teste Disc</h1>
+        <h2>Avaliação de Perfil Comportamental gratuito.</h2>
+    </header>
+    <section>
+        <div>
+            <div>
+                <img src="" alt="">
+            </div>
+            <div>
+                <header>
+                    <h3>Você está pronta(o) para conhecer seu perfil comportamental predominante?</h3>
+                </header>
+                <div>
+                    <div>
+                        <h4>DICAS:</h4>
+                        <ol>
+                            <li>Não há resultado bom ou ruim, por isso, seja sincera(o).</li>
+                            <li>Escolha um momento tranquilo para fazer o teste.</li>
+                            <li>Não pense muito, a primeira resposta geralmente será a mais correta.</li>
+                            <li>Responda pensando em quem você é e não em quem gostaria de ser.</li>
+                        </ol>
+                    </div>
+                    <div>
+                        <h4>Clique no botão para começar</h4>
+                        <button>Carregando...</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </section>
+
+</body>
+</html>
